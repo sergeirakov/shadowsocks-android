651701967af97a3b47f109c2453e6c59432b1ced
sergeirakov <sergei.rakov07@gmail.com>
2024-01-13 08:38:06 +0400

    Update version

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -56,8 +56,8 @@ fun Project.setupCore() {
     setupCommon()
     android.apply {
         defaultConfig {
-            versionCode = 202401042
-            versionName = "20240104_2"
+            versionCode = 202401061
+            versionName = "20240106_1"
         }
         compileOptions.isCoreLibraryDesugaringEnabled = true
         lint.apply {
